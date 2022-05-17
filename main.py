23e6ca5dad22feef58f75d104455520fb09231de
jingyeong <jingyeong0327@naver.com>
2022-05-17 22:25:05 +0900

    Corona manual

--- main.py
+++ main.py
@@ -27,17 +27,21 @@ def _total():
     info_total = soup.select_one('.total > li:nth-child(5) > div:nth-child(2) > span:nth-child(1)')
     return info_total.string
 
-token = os.environ.get('token',"5396200298:AAEhrqPUg_VX5UoRzuImCNoyhDE_i4nLgKQ")    # 토큰 넣기
-id = 5054586086                   # chat_id
+def _manual():
+    f = open("Corona_manual.txt", 'r')
+    data = f.read()
+    return data
+
+token = os.environ.get('token',"5322770624:AAGDUuFm7k50OHxwzfif3SqJuWv3dAN7GVc")    # 토큰 넣기
+id = 5316579447                  # chat_id
 bot = telegram.Bot(token)
 
 info_message = '* 생활 정보 알림이에 오신 것을 환영합니다.\n'\
                '* 필요한 정보를 입력해주세요.\n'\
                '* 코로나  : 일일 코로나 확진자 수 \n' \
                '* 평균확진자  : 7일 평균 확진자 수 \n' \
-               '* 누적확진자 : 현재까지의 누적 확진자 수'
-
-
+               '* 누적확진자 : 현재까지의 누적 확진자 수\n' \
+               '* 코로나지침 : 최근 코로나 방역지침 *\n '
 
 bot.sendMessage(chat_id=id, text=info_message)      # 봇이 시작될 때 출력
 
@@ -58,6 +62,10 @@ def handler(update, context):
     elif (user_text == "누적확진자"):
         covid_total = _total()
         bot.send_message(chat_id=id, text = "현재까지의 누적 확진자 수 : {} 명".format(covid_total))
+
+    elif (user_text == "코로나지침"):
+        covid_man = _manual()
+        bot.send_message(chat_id=id, text=covid_man)
 
 
 echo_handler = MessageHandler(Filters.text, handler)
